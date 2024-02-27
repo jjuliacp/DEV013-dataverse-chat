d23e273405ee6bc7b08054433587e6bd7bed9d01
Elisantib <elisa.santibaez1994@gmail.com>
2024-02-27 11:28:56 -0300

    avance en vista de chat grupal

--- src/views/ChatGrupal.js
+++ src/views/ChatGrupal.js
@@ -1,26 +1,16 @@
-/* import data from "../data/dataset.js"; */
+import data from "../data/dataset.js";
+/* import { renderData } from "../lib/dataFunctions.js"; */
 
 const ChatGrupal = () => {
   const chatG = document.createElement("section");
+  chatG.className = "view-ChGr";
 
-  /* const carta = data.find((x) => x.id === cardId); */
+  const chatGrupalText = document.createElement("div");
+  chatGrupalText.className = "text-ChGr";
+  const chatGrupalContact = document.createElement("div");
+  chatGrupalContact.className = "List-contact-ChGr";
 
-  chatG.innerHTML = `
-    <div class="chatGrupal-view">
-      <div class="container-gral-contact">
-        <div class="container-contact">
-          <h3 class="title-contact">Cartas Clow</h3>
-          <div class="contain-card">
-            <div class="avatar-container">
-              <img id="img2" class="img-Avatar" src="./img/sakura-inicio.jpg" alt="ejemploImg" />
-            </div>
-            <div class="avatar-name">
-              <h4 class="name-card">Sakura</h4>
-              <h5 class="description-card">Carta del agua</h5>
-            </div>
-          </div>
-        </div>
-      </div>
+  chatGrupalText.innerHTML = `
       <div class="chatGrupal">
         <div class="nav-chatGrupal">
           <h3 class="title-chatGrupal">Chat Grupal</h3>
@@ -28,9 +18,34 @@ const ChatGrupal = () => {
         <div class="window-chat">
           <textarea></textarea>          
         </div>
-      </div>    
-    </div>
-    `;
+      </div>
+  `;
+
+  data.forEach((carta) => {
+    const divData = document.createElement("div");
+    divData.className = "chatGrupal-view";
+
+    divData.innerHTML = `   
+      <div class="container-gral-contact">
+        <ul class="container-contact">
+          <li class="contain-card" itemscope itemtype="contact">
+            <dd class="avatar-container" itemprop="imagenUrl">
+              <img id="img2" class="img-Avatar" src=${carta.imageUrl} alt=${carta.name} />
+            </dd>
+            <dd class="avatar-name" itemprop="name">
+              <h4 class="name-card">${carta.name}</h4>
+              <h5 class="description-card">${carta.shortDescription}</h5>
+            </dd>
+          </li>
+        </ul>
+      </div>
+      `;
+
+    chatG.appendChild(chatGrupalText);
+    chatG.appendChild(chatGrupalContact);
+
+    chatGrupalContact.appendChild(divData);
+  });
 
   return chatG;
 };
