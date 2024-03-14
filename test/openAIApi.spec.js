224fb27add81810695821cc6e4fc30653e5197b8
Julia Cruz <131014598+jjuliacp@users.noreply.github.com>
2024-03-14 12:30:41 -0500

    terminar los test unitarios

--- test/openAIApi.spec.js
+++ test/openAIApi.spec.js
@@ -1,15 +1,22 @@
 /* eslint-disable */
 import { communicateWithOpenAI } from "../src/lib/openAIApi.js";
-
 describe("communicateWithOpenAI", () => {
-  test("communicateWithOpenAI debería enviar una solicitud a la Api de OpenAI y devolver los datos exitosamente", async () => {
-    const respuesta = {choices: [{ message: { content: 'Soy  una respuestaa' } }], ok: true}
-    global.fetch = jest.fn(() => Promise.resolve({ok:true, json: () => Promise.resolve(respuesta)})
-    
+  test("communicateWithOpenAI debería enviar una solicitud a la Api de OpenAI y devolver los datos exitosamente", () => {
+    const respuesta = { choices: [{ message: { content: 'Soy  una respuestaa' } }], ok: true }
+    global.fetch = jest.fn(() => Promise.resolve({ ok: true, json: () => Promise.resolve(respuesta) })
+
     )
-    const data = await communicateWithOpenAI("Este es un mensaje", {
+    const data = communicateWithOpenAI("Este es un mensaje", {
       id: "carta 1",
     });
-    expect(typeof(data)).toEqual('object');
+    expect(typeof (data)).toEqual('object');
+  });
+  test("deberia el error que devuelve",  () => {
+    global.fetch = jest.fn(() => Promise.reject(new Error("Error en la solicitud")));
+    try {
+       communicateWithOpenAI("Hola", { id: "carta 1" });
+    } catch (error) {
+      expect(error).toBeNull();
+    }
   });
 });
--- test/viewHome.spec.js
+++ test/viewHome.spec.js
@@ -27,3 +27,38 @@ describe("evento de resetear", () => {
     expect(selectFilter.value).toBe("");
   });
 });
+
+describe("verificar filter bar", () => {
+  it("verificar que se pinte los filtros", () => {
+    const home = Home();
+    const filterContainer = home.querySelector('.filterContainer');
+    const container = filterContainer.querySelector('.container-elementosFilter');
+    expect(container).toBeDefined();
+  });
+});
+
+describe("evento change de los filtros", () => {
+  it("se activa el evento de cambio al darle click", () => {
+    const data = [
+      { elementEsencial: 'value1' },
+      { elementEsencial: 'value2' }
+    ];
+    const selectElement = document.createElement('select');
+    selectElement.id = 'selectElement';
+    const cardsInfo = document.createElement('div');
+    cardsInfo.id = 'cardsInfo';
+
+    // Simula la función de filtrado de datos y de renderizado
+    const elementDataFilterMock = jest.fn((data, key, value) => data.filter(item => item[key] === value));
+    const renderDataMock = jest.fn(data => JSON.stringify(data));
+    selectElement.addEventListener('change', (event) => {
+      const filteredData = elementDataFilterMock(data, "elementEsencial", event.target.value);
+      cardsInfo.innerHTML = renderDataMock(filteredData);
+    });
+    const event = new Event('change');
+    selectElement.dispatchEvent(event)
+
+    expect(elementDataFilterMock).toHaveBeenCalledWith(data, "elementEsencial", expect.any(String))
+    expect(renderDataMock).toHaveBeenCalledWith(expect.any(Array));
+  });
+});
